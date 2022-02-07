055fceab7dda28ed8cde4758832e9afb97943265
maximihajlov <max@mer.pw>
2022-02-07 13:58:47 +0300

    dog-years is done

---
+++ dog-years.js
@@ -0,0 +1,13 @@
+function dogYears(planetName, ageSeconds) {
+    const planets = {
+        earth: 1,
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        mars: 1.8808158,
+        jupiter: 11.862615,
+        saturn: 29.447498,
+        uranus: 84.016846,
+        neptune: 164.79132,
+    }
+    return ageSeconds / (planets[planetName] * 31557600) * 7
+}
