001b38ea1bcb2b4209ed32b1594882f22ee971c7
maximihajlov <max@mer.pw>
2022-02-07 15:10:58 +0300

    sign - sameSign added and zero in sign fixed

--- sign.js
+++ sign.js
@@ -1,3 +1,9 @@
 function sign(num) {
-    return num / Math.abs(num)
+    if (num > 0) return 1
+    if (num < 0) return -1
+    return 0
+}
+
+function sameSign(val1, val2) {
+    return sign(val1) === sign(val2)
 }
