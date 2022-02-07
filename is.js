dcde84f4693dbaf2879d3905add584ac7fa10714
maximihajlov <max@mer.pw>
2022-02-07 15:32:24 +0300

    is - constant fix

--- is.js
+++ is.js
@@ -1,35 +1,43 @@
-is = {
-    num: function (value) {
-        return typeof value === "number"
-    },
-    nan: function (value) {
-        return isNaN(value)
-    },
-    str: function (value) {
-        return typeof value === "string"
-    },
-    bool: function (value) {
-        return typeof value === "boolean"
-    },
-    undef: function (value) {
-        return typeof value === "undefined"
-    },
-    def: function (value) {
-        return typeof value !== "undefined"
-    },
-    arr: function (value) {
-        return Array.isArray(arr)
-    },
-    obj: function (value) {
-        return typeof value === "object"
-    },
-    fun: function (value) {
-        return typeof value === "function"
-    },
-    truthy: function (value) {
-        return Boolean(value)
-    },
-    falsy: function (value) {
-        return Boolean(value)
-    }
+is.num = function (value) {
+    return typeof value === "number"
+}
+
+is.nan = function (value) {
+    return isNaN(value)
+}
+
+is.str = function (value) {
+    return typeof value === "string"
+}
+
+is.bool = function (value) {
+    return typeof value === "boolean"
+}
+
+is.undef = function (value) {
+    return typeof value === "undefined"
+}
+
+is.def = function (value) {
+    return typeof value !== "undefined"
+}
+
+is.arr = function (value) {
+    return Array.isArray(value)
+}
+
+is.obj = function (value) {
+    return typeof value === "object"
+}
+
+is.fun = function (value) {
+    return typeof value === "function"
+}
+
+is.truthy = function (value) {
+    return Boolean(value)
+}
+
+is.falsy = function (value) {
+    return Boolean(value)
 }
