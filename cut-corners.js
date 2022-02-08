b5fe8b5e5c4c5d7c76f1fcda86a5aca11c588176
maximihajlov <max@mer.pw>
2022-02-08 16:19:00 +0300

    cut-corners - nums<0 fixed

--- cut-corners.js
+++ cut-corners.js
@@ -1,17 +1,22 @@
 function round(num) {
-    if (num - (num | 0) >= 0.5) return (num | 0) + 1
-    return (num | 0)
+    if (num > 0) {
+        if (num - trunc(num) >= 0.5) return ceil(num)
+        return floor(num)
+    }
+    if (trunc(num) - num >= 0.5) return floor(num)
+    return ceil(num)
 }
 
 function ceil(num) {
-    return (num | 0) + 1
+    if (num > 0) return trunc(num) + 1
+    else return trunc(num)
 }
 
 function floor(num) {
-    return num | 0
+    if (num > 0) return trunc(num)
+    else return trunc(num) - 1
 }
 
 function trunc(num) {
     return num | 0
 }
-
