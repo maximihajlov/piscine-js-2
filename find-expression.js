fe634e806c5de8a62e68a68d841b8769321398b8
maximihajlov <max@mer.pw>
2022-02-08 20:42:52 +0300

    find-expression - eval changed to check from test

--- find-expression.js
+++ find-expression.js
@@ -1,3 +1,6 @@
+const add4 = '+4'
+const mul2 = '*2'
+
 function findExpression(to) {
     for (let i = 0; ; i++) {
         let seq = convertToBinary(i)
@@ -7,17 +10,25 @@ function findExpression(to) {
         // to / 4 > log_2(to) if to > 16, so max len can be set to 16/4 = 4
 
         if (seq.length > Math.max(to / 4, 4)) return undefined
-        seq = seq.replaceAll('0', add4 + ' ').replaceAll('1', mul2 + ' ').trimRight() //trimRight removes space at the end
-        if (eval(1 + seq) === to) return `1 ${seq}`
-    }
-}
+        seq = seq.split('0').join(add4 + ' ')
+        seq = seq.split('1').join(mul2 + ' ')
+        seq = `1 ${seq}`.trimRight()
 
-String.prototype.replaceAll = function replaceAll(search, replace) {
-    return this.split(search).join(replace);
+        if (count(seq) === to) return seq
+    }
 }
 
 function convertToBinary(num) {
     if (num > 0) return convertToBinary(parseInt(String(num / 2))) + (num % 2)
     return ''
+}
+
+function count(exp) {
+    return exp.slice(2)
+        .split(' ')
+        .reduce((total, op) => {
+            if (op === '+4') return total + 4
+            if (op === '*2') return total * 2
+        }, 1)
 }
 
