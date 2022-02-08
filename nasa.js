f88713d15802c6dbcb8fa038fcfb2cd756cc5f5f
maximihajlov <max@mer.pw>
2022-02-08 17:54:00 +0300

    nasa - fixed and done!

--- nasa.js
+++ nasa.js
@@ -1,12 +1,10 @@
 function nasa(N) {
     let res = ""
     for (let i = 1; i <= N; i++) {
-        if (i % 3 !== 0 && i % 5 !== 5) res += i
-        else {
-            if (i % 3 === 0) res += "NA"
-            if (i % 5 === 0) res += "SA"
-        }
-        res += " "
+        if (i % 3 === 0) res += "NA"
+        if (i % 5 === 0) res += "SA"
+        else if (!(i % 3 === 0)) res += i
+        if (i !== N) res += " "
     }
     return res
 }
