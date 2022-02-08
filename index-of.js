52ab4edd87de1279c1f1c940a7886b61d63b781b
maximihajlov <max@mer.pw>
2022-02-08 15:24:30 +0300

    index-of - result not found bug fixed

--- index-of.js
+++ index-of.js
@@ -2,18 +2,20 @@ function indexOf(arr, el, index = 0) {
     for (let i = index; i < arr.length; i++) {
         if (arr[i] === el) return i
     }
+    if (index !== 0) return indexOf(arr, el)
     return -1
 }
 
 function lastIndexOf(arr, el, index = 0) {
     let result = -1
     for (let i = index; i < arr.length; i++) {
-        if (arr[i] === el) result = i - index
+        if (arr[i] === el) result = i
     }
-    return result
+    if (result !== -1) return result
+    if (index !== 0) return lastIndexOf(arr, el)
+    return -1
 }
 
 function includes(arr, el) {
     return indexOf(arr, el) !== -1;
 }
-
