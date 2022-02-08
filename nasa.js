795b7ee6e8be19bbcd938174fb07d49da9560b72
maximihajlov <max@mer.pw>
2022-02-08 17:46:55 +0300

    nasa - starting from 1, %3 and %5 numbers are now replaced by NA and SA instead of adding

--- nasa.js
+++ nasa.js
@@ -1,9 +1,11 @@
 function nasa(N) {
     let res = ""
-    for (let i = 0; i <= N; i++) {
-        res += i
-        if (i % 3 === 0) res += "NA"
-        if (i % 5 === 0) res += "SA"
+    for (let i = 1; i <= N; i++) {
+        if (i % 3 !== 0 && i % 5 !== 5) res += i
+        else {
+            if (i % 3 === 0) res += "NA"
+            if (i % 5 === 0) res += "SA"
+        }
         res += " "
     }
     return res
