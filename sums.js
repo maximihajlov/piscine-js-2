510b1b447f0a49a4b129ecbdb12d0485037b481f
maximihajlov <max@mer.pw>
2022-02-09 20:32:45 +0300

    sums - done. sort added

--- sums.js
+++ sums.js
@@ -18,5 +18,24 @@ function sums(num) {
         }
     }
 
-    return result.slice(0, -1) //last element is always [num]
+    return result.slice(0, -1).sort() //last element is always [num]
 }
+
+const $7 = [
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 2],
+    [1, 1, 1, 1, 3],
+    [1, 1, 1, 2, 2],
+    [1, 1, 1, 4],
+    [1, 1, 2, 3],
+    [1, 1, 5],
+    [1, 2, 2, 2],
+    [1, 2, 4],
+    [1, 3, 3],
+    [1, 6],
+    [2, 2, 3],
+    [2, 5],
+    [3, 4],
+]
+
+console.log(sums(7))
