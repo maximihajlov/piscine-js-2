a20c6c82afcbf2b393af2711f5db7e4e15f55a89
maximihajlov <max@mer.pw>
2022-02-09 20:20:09 +0300

    sums - done

--- sums.js
+++ sums.js
@@ -1,11 +1,22 @@
 function sums(num) {
-    function get(n, limit) {
-        if (limit > n) return get(n, n)
-        if (limit > 0) return get(n, limit - 1) + get(n - limit, limit)
-        if (n === 0 && limit === 0) return 1
+    let part = []
+    for (let i = 0; i < num; i++) {
+        part.push(1)
+    } // generating [1,1,1,...,1]
+
+    let result = [part.slice()]
+
+    while (part[0] !== num) {
+        let min = Math.min(...part.slice(0, -1))
+        for (let i = 0; i < part.length - 1; i++) {
+            if (part[i] === min) {
+                part[i]++
+                part[part.length - 1]--
+                if (part[part.length - 1] === 0) part.pop()
+                result.push(part.slice())
+            }
+        }
     }
 
-    return get(num, num)
+    return result.slice(0, -1)
 }
-
-console.log(sums(4))
