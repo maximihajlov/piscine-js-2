191b8d357dfc0d1df721e2b35f2041f08014f2d9
maximihajlov <max@mer.pw>
2022-02-10 13:46:24 +0300

    date-is - validation added

--- date-is.js
+++ date-is.js
@@ -4,17 +4,27 @@ function isValid(date) {
 }
 
 function isAfter(date1, date2) {
+    if (!isValid(date1)) return false
+    if (!isValid(date2)) return false
+
     return date1 > date2
 }
 
 function isBefore(date1, date2) {
+    if (!isValid(date1)) return false
+    if (!isValid(date2)) return false
+
     return date1 < date2
 }
 
 function isFuture(date) {
+    if (!isValid(date)) return false
+
     return date > Date.now()
 }
 
 function isPast(date) {
+    if (!isValid(date)) return false
+
     return date < Date.now()
 }
