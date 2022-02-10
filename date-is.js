845417199c528cd71758ac212cf5eed697cb2fdc
maximihajlov <max@mer.pw>
2022-02-10 13:37:03 +0300

    date-is - fixed

--- date-is.js
+++ date-is.js
@@ -1,31 +1,20 @@
 function isValid(date) {
-    date = new Date(date)
     if (Object.prototype.toString.call(date) !== "[object Date]") return false
     return !isNaN(date);
 }
 
 function isAfter(date1, date2) {
-    date1 = new Date(date1)
-    date2 = new Date(date2)
-    if (!isValid(date1) || !isValid(date2)) return false
-    return date1.getTime() > date2.getTime()
+    return date1 > date2
 }
 
 function isBefore(date1, date2) {
-    date1 = new Date(date1)
-    date2 = new Date(date2)
-    if (!isValid(date1) || !isValid(date2)) return false
-    return date1.getTime() < date2.getTime()
+    return date1 < date2
 }
 
 function isFuture(date) {
-    date = new Date(date)
-    if (!isValid(date)) return false
-    return date.getTime() > Date.now()
+    return date > Date.now()
 }
 
 function isPast(date) {
-    date = new Date(date)
-    if (!isValid(date)) return false
-    return date.getTime() < Date.now()
+    return date < Date.now()
 }
