81713d74275bd24d475fd1758ca8c655a568cd4b
maximihajlov <max@mer.pw>
2022-02-11 22:07:28 +0300

    pick-and-click - done

---
+++ pick-and-click.js
@@ -0,0 +1,56 @@
+let hsl
+export const pick = (e) => {
+    if (!e) return
+    let hue = e.x
+    let lum = e.y
+    hue = Math.round(hue / window.innerWidth * 360)
+    lum = Math.round(lum / window.innerHeight * 100)
+    hsl = `hsl(${hue}, 50%, ${lum}%)`
+
+    tHue.innerHTML = `hue\n${hue}`
+    tLum.innerHTML = `${lum}\nluminocity`
+    tHsl.innerHTML = hsl
+    axisX.setAttribute('x1', e.x)
+    axisX.setAttribute('x2', e.x)
+
+    axisY.setAttribute('y1', e.y)
+    axisY.setAttribute('y2', e.y)
+
+    document.body.style.background = hsl
+}
+
+document.addEventListener('mousemove', pick)
+document.addEventListener('click', () => navigator.clipboard.writeText(hsl))
+
+
+let tHsl, tHue, tLum
+
+let axisX, axisY
+document.addEventListener("DOMContentLoaded", function (event) {
+    tHsl = document.createElement('div')
+    tHue = document.createElement('div')
+    tLum = document.createElement('div')
+    tHsl.classList.add('text')
+    tHue.classList.add('text')
+    tLum.classList.add('text')
+
+    axisX = document.createElementNS("http://www.w3.org/2000/svg", 'line')
+    axisY = document.createElementNS("http://www.w3.org/2000/svg", 'line')
+    axisX.id = 'axisX'
+    axisX.id = 'axisY'
+
+    axisX.setAttribute('y1', '-10000')
+    axisX.setAttribute('y2', '10000')
+
+    axisY.setAttribute('x1', '-10000')
+    axisY.setAttribute('x2', '10000')
+
+    tHsl.classList.add('hsl')
+    tHue.classList.add('hue')
+    tLum.classList.add('luminosity')
+
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.append(axisX, axisY)
+
+    document.body.append(tHsl, tHue, tLum, svg)
+});
