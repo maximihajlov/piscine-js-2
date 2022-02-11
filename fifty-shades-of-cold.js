d6516ecd0a1d4a3117a67156f16de61370645b8b
maximihajlov <max@mer.pw>
2022-02-11 19:20:59 +0300

    fifty-shades-of-cold - done

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,30 @@
+import {colors} from "./fifty-shades-of-cold.data.js";
+
+export const generateClasses = () => {
+    let style = document.createElement('style')
+
+    colors.forEach(col => {
+        style.innerHTML += `.${col} {background: ${col}}`
+    })
+
+    document.head.append(style)
+}
+
+export const generateColdShades = () => {
+    let good = colors.filter(col => {
+        return col.match(/(aqua|blue|turquoise|green|cyan|navy|purple)/)
+    })
+    good.forEach(col => {
+        let div = document.createElement('div')
+        div.classList.add(col)
+        div.innerHTML = col
+        document.body.append(div)
+    })
+}
+
+export const choseShade = (color) => {
+    Array.from(document.getElementsByTagName('div')).forEach(div => {
+        div.classList = color
+        div.innerHTML = color
+    })
+}
