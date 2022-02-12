2dbe43f7a22c1e8ab3ab80b21eb5c59e416e95b8
maximihajlov <max@mer.pw>
2022-02-12 12:56:15 +0300

    using-map - done

---
+++ using-map.js
@@ -0,0 +1,36 @@
+function citiesOnly(arr) {
+    return arr.map(el => el.city)
+}
+
+function upperCasingStates(arr) {
+    return arr.map(state => {
+        return state.split(' ').map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1)
+        }).join(' ')
+    })
+}
+
+function fahrenheitToCelsius(arr) {
+    return arr.map(el => {
+        return `${Math.round((Number(el.slice(0, -2)) - 32) * (5 / 9))}°C`
+    })
+}
+
+function tempForecasts(arr) {
+    return arr.map(el => {
+        let temp = `${Math.round((Number(el['temperature'].slice(0, -2)) - 32) * (5 / 9))}°Celsius`
+        let state = el['state'].split(' ').map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1)
+        }).join(' ')
+        return `${temp} in ${el.city}, ${state}`
+    })
+}
+
+console.log(tempForecasts([
+    {
+        city: 'Pasadena',
+        temperature: ' 101 °F',
+        state: 'california',
+        region: 'West',
+    },
+]))
